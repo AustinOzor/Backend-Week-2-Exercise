9a2f9cf801ebf9a4044157ccf1f21a80b2cde70a
Austin Ozor <augustineozor@gmail.com>
2023-01-21 01:40:53 +0300

    backend exercise two update

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ const handleLogInRequest = (req, res) => {
     res.send("<h2>Welcome to the login page</h2>")
 };
 
+const handleSignUpRequest = (req, res) => {
+    res.send("<h3>You are here at SignUP</h3>")
+};
 const handleProfileRequest = (req, res) => {
     res.send("<h3>You are here</h3>")
 };
 
-server.use('/', handleHomeRequest);
+server.use('/profile', handleProfileRequest);
+server.use('/signup', handleSignUpRequest);
 server.use('/login', handleLogInRequest);
-server.use('/profile',handleProfileRequest);
+server.use('/', handleHomeRequest);
 
 server.use(handleAllRequests);
 
